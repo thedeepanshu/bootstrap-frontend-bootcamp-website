c49b82ec8f4db6157029f911191e952ba1fde1de
thedeepanshu <deepanshusaini971@gmail.com>
2021-07-02 00:04:38 +0530

    Initial commit

---
+++ static/js/mapbox.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZGV2ZGVlIiwiYSI6ImNrcWtzb3BmczBvamMyb254aXk4NDJlZm0ifQ.5nD2x5D3un55EDklU84PaQ";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-71.060982, 42.35725],
+  zoom: 18,
+});
